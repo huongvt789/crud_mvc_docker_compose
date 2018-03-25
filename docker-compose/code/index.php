74f21be0df933229061007e822e138a866fe3fd8
VuHuong <huongvt52@wru.vn>
2018-03-25 22:36:33 +0700

    docker-compose_crud

--- docker-compose/code/index.php
+++ docker-compose/code/index.php
@@ -1,3 +1,9 @@
 <?php 
-echo "Tao la Huong";
+
+
+   include_once('crud_mvc_php/Views/createStudent.php');
+   include_once('crud_mvc_php/Views/listStudent.php');
+   include_once('crud_mvc_php/Views/editStudent.php');
+
+
 ?>
